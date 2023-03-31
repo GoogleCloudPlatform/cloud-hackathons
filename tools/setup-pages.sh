6bf88660ed93895cded8831eb83fa199199d4908
Murat Eken <meken@google.com>
2023-03-31 12:34:35 +0200

    pre-processing md files...

--- tools/setup-pages.sh
+++ tools/setup-pages.sh
@@ -4,6 +4,16 @@ TARGET=dist
 
 mkdir -p ${TARGET}/hacks
 rsync -av --exclude=${TARGET} --exclude=_site --exclude=tools --exclude=hacks --exclude=.git . ${TARGET}
+
+NOTE="\\1> <span class=\"info\">{% octicon info %} Note</span>\\2"
+WARN="\\1> <span class=\"alert\">{% octicon alert %} Warning</span>\\2"
+for FAQ in $(ls $TARGET/faq/*.md); do
+    sed -i -r \
+        -e "s|(\s*)> \*\*Note\*\*(.*)|$NOTE|g" \
+        -e "s|(\s*)> \*\*Warning\*\*(.*)|$WARN|g" \
+        $1
+done
+
 for HACK in $(ls hacks); do
     GEN=${TARGET}/hacks/${HACK}
     IMG=hacks/${HACK}/images
