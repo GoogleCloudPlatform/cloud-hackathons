26365224f11f1cfdc3676de19e36bd5a13267f49
Murat Eken <meken@google.com>
2023-05-08 11:29:16 +0200

    creating service agent explicitly...

--- hacks/mlops-on-gcp/artifacts/main.tf
+++ hacks/mlops-on-gcp/artifacts/main.tf
@@ -2,14 +2,17 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.57.0"
+      version = "4.63.1"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "4.63.1"
     }
   }
 }
 
 locals {
-  build_default_sa      = "${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
-  monitoring_default_sa = "service-${data.google_project.project.number}@gcp-sa-monitoring-notification.iam.gserviceaccount.com"
+  build_default_sa = "${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 }
 
 provider "google" {
@@ -17,11 +20,16 @@ provider "google" {
   region  = var.gcp_region
 }
 
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
 data "google_project" "project" {}
 
 resource "google_project_service" "compute_api" {
-  service                    = "compute.googleapis.com"
-  disable_dependent_services = true
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
 }
 
 resource "google_project_service" "notebooks_api" {
@@ -45,8 +53,8 @@ resource "google_project_service" "scheduler_api" {
 }
 
 resource "google_project_service" "pubsub_api" {
-  service                    = "pubsub.googleapis.com"
-  disable_dependent_services = true
+  service            = "pubsub.googleapis.com"
+  disable_on_destroy = false
 }
 
 data "google_compute_default_service_account" "gce_default" {
@@ -82,22 +90,22 @@ resource "google_service_account_iam_member" "gce_default_account_user_iam" {
   member             = "serviceAccount:${local.build_default_sa}"
 }
 
-# fake email setup to force monitoring service acccount creation
-resource "google_monitoring_notification_channel" "basic" {
-  display_name = "Test Notification Channel"
-  type         = "email"
-  labels = {
-    email_address = "noreply@example.com"
-  }
-  force_delete = false
+resource "google_project_service_identity" "monitoring_default_sa" {
+  provider = google-beta
+
+  project = data.google_project.project.project_id
+  service = "monitoring.googleapis.com"
 }
 
 resource "google_project_iam_member" "monitoring_default_iam" {
   project = var.gcp_project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${local.monitoring_default_sa}"
+  for_each = toset([
+    "roles/pubsub.publisher",
+    "roles/monitoring.notificationServiceAgent"
+  ])
+  role   = each.key
+  member = "serviceAccount:${google_project_service_identity.monitoring_default_sa.email}"
   depends_on = [
-    google_project_service.pubsub_api,
-    google_monitoring_notification_channel.basic
+    google_project_service.pubsub_api
   ]
 }
