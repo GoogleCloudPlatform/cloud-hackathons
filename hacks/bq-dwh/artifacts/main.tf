30d877e6c6db74ea828be6c1f227370a3d8a7bde
Murat Eken <meken@google.com>
2024-09-04 17:18:15 +0200

    getting the data from an alternative location...

--- hacks/bq-dwh/artifacts/main.tf
+++ hacks/bq-dwh/artifacts/main.tf
@@ -200,7 +200,7 @@ resource "google_compute_instance" "startup_vm" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "debian-cloud/debian-12"
     }
   }
 
