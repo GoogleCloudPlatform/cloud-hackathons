b1568dd21ab82a7ce83423243c775851f45d9832
Gino Filicetti <ginof@google.com>
2024-11-13 10:35:23 -0500

    adding some context to our implementation

--- hacks/ccai-virtual-agents/artifacts/function/main.py
+++ hacks/ccai-virtual-agents/artifacts/function/main.py
@@ -1,6 +1,7 @@
 import json
 import random
 
+# original implementation was: https://xkcd.com/221/
 def on_request(request) -> str:
-	days: int = random.randint(1, 100)
+	days: int = random.randint(1, 20)
 	return json.dumps({"vacation_days_left": days})
