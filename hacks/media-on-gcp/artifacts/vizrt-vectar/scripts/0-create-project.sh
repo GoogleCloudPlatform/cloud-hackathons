11584f9865995579fc44913457124f75f68a68b6
Gino Filicetti <ginof@google.com>
2025-06-26 17:47:31 +0000

    fixed typo and using billing account now

--- hacks/media-on-gcp/artifacts/vizrt-vectar/scripts/0-create-project.sh
+++ hacks/media-on-gcp/artifacts/vizrt-vectar/scripts/0-create-project.sh
@@ -4,7 +4,7 @@ ACCOUNT_ID=${2:-$(gcloud billing accounts list --format="value(ACCOUNT_ID)" --li
 
 # create the project and set it
 gcloud projects create $PROJECT
-gcluod config set project $PROJECT
+gcloud config set project $PROJECT
 
 # attach the billing account
-gcloud billing projects link $PROJECT --billing-account=
+gcloud billing projects link $PROJECT --billing-account=$ACCOUNT_ID
