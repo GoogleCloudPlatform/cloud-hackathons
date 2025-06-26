2c55c03385e7475aafc070885a29b843069d49ee
Gino Filicetti <ginof@google.com>
2025-06-26 13:42:13 -0400

    added a create project script

---
+++ hacks/media-on-gcp/artifacts/vizrt-vectar/scripts/0-create-project.sh
@@ -0,0 +1,10 @@
+PROJECT=${1:-"ghacks-vizrt-servers"}
+ACCOUNT_ID=${2:-$(gcloud billing accounts list --format="value(ACCOUNT_ID)" --limit=1)}
+
+
+# create the project and set it
+gcloud projects create $PROJECT
+gcluod config set project $PROJECT
+
+# attach the billing account
+gcloud billing projects link $PROJECT --billing-account=
