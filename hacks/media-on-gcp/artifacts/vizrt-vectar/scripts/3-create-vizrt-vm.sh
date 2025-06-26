50219d7e621d09762a1afb6779a7883d78ad4e05
Gino Filicetti <ginof@google.com>
2025-06-26 16:50:34 -0400

    root disk must be the size of the image: ie: 50GB

--- hacks/media-on-gcp/artifacts/vizrt-vectar/scripts/3-create-vizrt-vm.sh
+++ hacks/media-on-gcp/artifacts/vizrt-vectar/scripts/3-create-vizrt-vm.sh
@@ -12,7 +12,7 @@ gcloud compute instances create $VM_NAME \
 	--network-interface=address=$IP,network-tier=PREMIUM,nic-type=GVNIC,stack-type=IPV4_ONLY,subnet=$REGION \
 	--accelerator=count=1,type=nvidia-l4-vws \
 	--tags=http-server,https-server,lb-health-check \
-	--create-disk=auto-delete=yes,boot=yes,device-name=$VM_NAME,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20250613,mode=rw,size=70,type=pd-balanced \
+	--create-disk=auto-delete=yes,boot=yes,device-name=$VM_NAME,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20250613,mode=rw,size=50,type=pd-balanced \
 	--create-disk=auto-delete=yes,device-name=disk-2,mode=rw,name=$VM_NAME-disk-2,size=200,type=pd-balanced \
 	--create-disk=auto-delete=yes,device-name=disk-3,mode=rw,name=$VM_NAME-disk-3,size=500,type=pd-ssd \
 	--shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring \
