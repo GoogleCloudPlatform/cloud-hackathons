334f605a898805f11626e964129770892eb07277
Chris H <champart@google.com>
2025-08-24 23:55:30 +0000

    champart: Added a new clouddns folder with main.tf, outputs.tf and variables.tf

---
+++ hacks/media-on-gcp/artifacts/basics/clouddns/main.tf
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "dns-public-zone" {
+  source  = "terraform-google-modules/cloud-dns/google"
+  version = "~> 5.0"
+
+  project_id                         = var.project_id
+  type                               = "public"
+  name                               = var.name
+  domain                             = var.domain
+  labels                             = var.labels
+  private_visibility_config_networks = var.network_self_links
+
+  enable_logging = true
+
+  recordsets = [
+    {
+      name = ""
+      type = "A"
+      ttl  = 300
+      records = [
+        "34.120.52.154",
+      ]
+    },
+    {
+      name = "*"
+      type = "CNAME"
+      ttl  = 300
+      records = [
+        "viper.media.ghacks.dev.",
+      ]
+    },
+    {
+      name = ""
+      type = "NS"
+      ttl  = 300
+      records = [
+        "ns.${var.domain}",
+      ]
+    },
+  ]
+}
