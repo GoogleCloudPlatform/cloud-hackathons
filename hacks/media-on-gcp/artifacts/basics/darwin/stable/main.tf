3bb0ae5028a113cd4cd6b6f33afb97e11a5f5c2f
Gino Filicetti <ginof@google.com>
2025-08-27 12:36:23 -0400

    Darwin no longer needs a startup script

--- hacks/media-on-gcp/artifacts/basics/darwin/stable/main.tf
+++ hacks/media-on-gcp/artifacts/basics/darwin/stable/main.tf
@@ -15,23 +15,6 @@ module "darwin" {
   sub_networks        = var.sub_networks
   external_ips        = var.external_ips
 
-  startup_script = <<-EOT
-    #!/bin/bash
-    set -e
-
-    echo ">>> Starting startup script for tx-darwin..."
-
-    # Data Sync
-    mkdir -p /var/node
-    cd /var/node
-    gsutil cp gs://ghacks-media-on-gcp-private/tx-deploy.tar.gz /var/node
-    tar xvzf tx-deploy.tar.gz
-    cd /var/node/
-    chmod +x darwin-init.sh
-    sudo ./darwin-init.sh
-    echo ">>> Startup script for tx-darwin: finished."
-  EOT
-
   named_ports = [{
     name = "https"
     port = 443
