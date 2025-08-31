a4d66fe6937ccb041fbda5004ebfdf1771262c64
Gino Filicetti <ginof@google.com>
2025-08-31 11:56:57 -0400

    Spliting out the adding of permissions from the CR service deployment

--- hacks/media-on-gcp/artifacts/apps/dns-modifier/deploy-service.sh
+++ hacks/media-on-gcp/artifacts/apps/dns-modifier/deploy-service.sh
@@ -4,9 +4,6 @@ SERVICE_NAME=${1:-dns-modifier}
 PROJECT_ID=${2:-`gcloud config get-value project`}
 REGION=${3:-us-central1}
 
-PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
-SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
-
 echo "Deploying the service: $SERVICE_NAME..."
 
 gcloud run deploy "$SERVICE_NAME" \
@@ -14,11 +11,3 @@ gcloud run deploy "$SERVICE_NAME" \
     --platform managed \
     --region "$REGION" \
     --allow-unauthenticated
-
-echo "Granting DNS Administrator role to the default compute service account..."
-
-# Get the project ID and number to construct the default service account email
-# Grant the DNS Administrator role to the service account
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member="serviceAccount:${SERVICE_ACCOUNT}" \
-    --role="roles/dns.admin"
