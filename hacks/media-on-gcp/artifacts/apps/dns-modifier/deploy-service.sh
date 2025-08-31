769fdb46d02ee7c466351c1fefca1b892a1f305d
Gino Filicetti <ginof@google.com>
2025-08-31 11:43:39 -0400

    giving the computer SA dns admin perms

--- hacks/media-on-gcp/artifacts/apps/dns-modifier/deploy-service.sh
+++ hacks/media-on-gcp/artifacts/apps/dns-modifier/deploy-service.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 SERVICE_NAME=${1:-dns-modifier}
-REGION=${2:-us-central1}
+PROJECT_ID=${2:-`gcloud config get-value project`}
+REGION=${3:-us-central1}
+
+PROJECT_NUMBER=$(gcloud projects describe "$PROJECT_ID" --format="value(projectNumber)")
+SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+
+echo "Deploying the service: $SERVICE_NAME..."
 
 gcloud run deploy "$SERVICE_NAME" \
     --source . \
     --platform managed \
     --region "$REGION" \
     --allow-unauthenticated
+
+echo "Granting DNS Administrator role to the default compute service account..."
+
+# Get the project ID and number to construct the default service account email
+# Grant the DNS Administrator role to the service account
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    --member="serviceAccount:${SERVICE_ACCOUNT}" \
+    --role="roles/dns.admin"
