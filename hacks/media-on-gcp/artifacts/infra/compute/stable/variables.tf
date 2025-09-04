b8c387b826243efc0080e34108ec82ffb53dbc29
Michael Bychkowski <mbychkowski@google.com>
2025-09-04 21:50:12 +0000

    Full acces scope by default for vms

--- hacks/media-on-gcp/artifacts/infra/compute/stable/variables.tf
+++ hacks/media-on-gcp/artifacts/infra/compute/stable/variables.tf
@@ -116,12 +116,7 @@ variable "service_account_scopes" {
   description = "The list of scopes to be made available for the service account."
   type        = list(string)
   default = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append"
+    "https://www.googleapis.com/auth/cloud-platform"
   ]
 }
 
