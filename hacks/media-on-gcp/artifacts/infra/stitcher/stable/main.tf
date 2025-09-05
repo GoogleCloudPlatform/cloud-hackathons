b90c4ae4ad92e247c5f00937be3f0667e6cfa2b0
Michael Bychkowski <mbychkowski@google.com>
2025-09-05 01:00:17 +0000

    Change machine shape for stitcher

--- hacks/media-on-gcp/artifacts/infra/stitcher/stable/main.tf
+++ hacks/media-on-gcp/artifacts/infra/stitcher/stable/main.tf
@@ -6,7 +6,7 @@ module "compute" {
   instance_group_name  = "stitch-machine-mig"
   base_instance_name   = "stitch-machine"
   target_size          = 1
-  machine_type         = "e2-medium"
+  machine_type         = "c4d-standard-2"
   source_image         = "projects/media-on-gcp-storage/global/images/stitch-machine"
   boot_disk_type       = "pd-balanced"
   boot_disk_size       = 50
