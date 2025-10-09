510a1b79c34fb6ac58bb1a97ba0c179954c8ff46
Murat Eken <meken@google.com>
2025-10-09 19:49:19 +0200

    basic TF...

--- hacks/genmedia-on-gcp/artifacts/main.tf
+++ hacks/genmedia-on-gcp/artifacts/main.tf
@@ -11,7 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "google_project_service" "compute_api" {
-  service            = "compute.googleapis.com"
-  disable_on_destroy = false  
+resource "google_project_service" "default" {
+  project = var.gcp_project_id
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com",
+    "compute.googleapis.com",
+    "aiplatform.googleapis.com"
+  ])
+  service = each.key
+
+  disable_on_destroy = false
 }
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "${var.gcp_project_id}"
+  location                    = var.gcp_region
+  uniform_bucket_level_access = true
+}
+
